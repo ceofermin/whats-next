2f8ce25c006aff851e6b147ba7871e78938178f2
Logan Fermin <info@mtfcuh.com>
2022-12-10 20:42:12 -0500

    Refactor progressbar component
    create a more efficient method for generating the hoursPassed array and organize the component logic

--- src/app/components/progressbar/progressbar.component.ts
+++ src/app/components/progressbar/progressbar.component.ts
@@ -7,36 +7,21 @@ import { interval } from 'rxjs';
   styleUrls: ['./progressbar.component.sass']
 })
 export class ProgressbarComponent implements OnInit {
-  id: any;
   now = new Date();
-  getCurrentTime(): string {
-    let rValue: string = "";
-    let hours: number = this.now.getHours();
-    rValue += `${hours < 10 ? '0' : ''}${hours}:`;
-    rValue += `${this.now.getMinutes() < 10 ? '0' : ''}${this.now.getMinutes()}:`;
-    rValue += `${this.now.getSeconds() < 10 ? '0' : ''}${this.now.getSeconds()}`;
-    return rValue;
-  }
-  getHoursPassed(): boolean[] {
-    let rValue = [];
-    let passed = this.now.getHours();
-    for (let hours = 24; hours > 0; --hours) {
-      rValue.push(passed > 0);
-      --passed;
-    }
-    return rValue;
-  }
-  time: string = this.getCurrentTime();
-  hoursPassed: boolean[] = this.getHoursPassed();
+  hours: boolean[];
 
-  constructor() {}
+  getHoursPassed(): boolean[] {
+    return [...Array(24)].map((e, i) => this.now.getHours() > i);
+  }
+
+  constructor() {
+    this.hours = this.getHoursPassed();
+  }
 
   ngOnInit(): void {
-    const secondPassed = interval(1000);
-    secondPassed.subscribe(() => {
+    interval(1000).subscribe(() => {
       this.now = new Date();
-      this.time = this.getCurrentTime();
-      this.hoursPassed = this.getHoursPassed();
+      this.hours = this.getHoursPassed();
     });
   }
 }
